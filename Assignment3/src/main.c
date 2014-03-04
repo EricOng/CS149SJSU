0b39ad0e0a85de70492c105d20acfd99564a3205
Chris Sharpe <csha9933@yahoo.com>
2014-03-03 17:26:45 -0800

    Added comments to 'main.c'

--- Assignment3/src/main.c
+++ Assignment3/src/main.c
@@ -9,13 +9,15 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2)
-    {
+    if(argc != 2) // Test for the right number of command-line arguments
+    { // If the count is wrong, print a usage mesage instead.
         printf("%s requires ONE command-line argument\n", argv[0]);
         printf("(the number of customers per seller).\n");
         return 0;
     }
     
+    // Convert the number of customers passed to the program to an
+    // int for later use
     const int NUMBER_OF_CUSTOMERS = atoi(argv[1]);
     
     printf("Proceeding with %d customers per seller.\n", NUMBER_OF_CUSTOMERS);
