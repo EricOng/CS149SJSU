fd5aaf3000e1dee02ec4442e8f9f12abf046cfed
eo <eo@debian>
2014-03-05 23:25:36 -0800

    Added lfu and mfu

--- Assignment4/src/page.c
+++ Assignment4/src/page.c
@@ -63,6 +63,89 @@ void removeFirstPage(Page* memory, Page* pages, int memSize)
 	}
 }
 
+int findLFU(Page* memory, int memSize)
+{
+	int i;
+	int lfu = memory[0].accessCount;
+	int lfu_index = 0;
+	for(i = 0; i < memSize; i++)
+	{
+		printf("page %d has freq %d\n", memory[i].pid, memory[i].accessCount);
+		if(memory[i].accessCount <= lfu)
+		{
+			lfu = memory[i].accessCount;
+			lfu_index = i;
+		}
+	}
+	printf("Least freq page %d \n", memory[lfu_index].pid);
+	return lfu_index;
+}
+int findMFU(Page* memory, int memSize)
+{
+	int i;
+	int lfu = memory[0].accessCount;
+	int lfu_index = 0;
+	for(i = 0; i < memSize; i++)
+	{
+		printf("page %d has freq %d\n", memory[i].pid, memory[i].accessCount);
+		if(memory[i].accessCount >= lfu)
+		{
+			lfu = memory[i].accessCount;
+			lfu_index = i;
+		}
+	}
+	printf("Most freq page %d \n", memory[lfu_index].pid);
+	return lfu_index;
+}
+
+void removeLFUPage(Page* memory, Page* pages, int memSize)
+{
+	int i;
+	int lfu_Index = findLFU(memory, memSize);
+	for(i = 0; i < 10; i++)
+	{
+		if(memory[lfu_Index].pid == pages[i].pid)
+		{			
+			pages[i].inMemory = false;
+			break;		
+		}
+	}	
+	printf("Removed page %d\n", memory[lfu_Index].pid);
+	int count = lfu_Index;
+	while(count != memSize)
+	{
+		if(count + 1 < memSize)
+		{	
+			memory[count] = memory[count + 1];
+		}
+		count++;
+	}
+}
+
+void removeMFUPage(Page* memory, Page* pages, int memSize)
+{
+	int i;
+	int mfu_Index = findMFU(memory, memSize);
+	for(i = 0; i < 10; i++)
+	{
+		if(memory[mfu_Index].pid == pages[i].pid)
+		{			
+			pages[i].inMemory = false;
+			break;		
+		}
+	}	
+	printf("Removed page %d\n", memory[mfu_Index].pid);
+	int count = mfu_Index;
+	while(count != memSize)
+	{
+		if(count + 1 < memSize)
+		{	
+			memory[count] = memory[count + 1];
+		}
+		count++;
+	}
+}
+
 int removeLRUPage(Page* memory, Page* pages, int memSize)
 {
 	int i = 0;
@@ -110,6 +193,78 @@ bool addToMemory(Page* memory, Page* page, int memSize, int cIndex, int pIndex)
 		}
 		return true;
 	}
+	return false;
+}
+
+bool addToLFUMemory(Page* memory, Page* page, int memSize, int cIndex, int pIndex)
+{
+	if(!page[pIndex].inMemory)
+	{	
+		int i;
+		if(cIndex < memSize)
+		{
+			memory[cIndex] = page[pIndex];
+			page[pIndex].inMemory = true;
+			printf("Adding  page %d\n", page[pIndex].pid);
+		}
+		else{
+			removeLFUPage(memory, page, memSize);
+			memory[memSize-1] = page[pIndex];	
+			page[pIndex].inMemory = true;
+			printf("Adding  page %d\n", page[pIndex].pid);
+		}
+		return true;
+	}
+	else{
+		int i;
+		int j;
+		
+		for(j = 0;j < memSize; j++)
+		{
+			if(memory[j].pid == page[pIndex].pid)
+			{
+				memory[j].accessCount += 1;
+				page[pIndex].accessCount += 1;
+			}
+		}
+	}
+		printf("Page %d accessed %d -> %d\n", page[pIndex].pid, page[pIndex].accessCount - 1, page[pIndex].accessCount);
+	return false;
+}
+
+bool addToMFUMemory(Page* memory, Page* page, int memSize, int cIndex, int pIndex)
+{
+	if(!page[pIndex].inMemory)
+	{	
+		int i;
+		if(cIndex < memSize)
+		{
+			memory[cIndex] = page[pIndex];
+			page[pIndex].inMemory = true;
+			printf("Adding  page %d\n", page[pIndex].pid);
+		}
+		else{
+			removeMFUPage(memory, page, memSize);
+			memory[memSize-1] = page[pIndex];	
+			page[pIndex].inMemory = true;
+			printf("Adding  page %d\n", page[pIndex].pid);
+		}
+		return true;
+	}
+	else{
+		int i;
+		int j;
+		
+		for(j = 0;j < memSize; j++)
+		{
+			if(memory[j].pid == page[pIndex].pid)
+			{
+				memory[j].accessCount += 1;
+				page[pIndex].accessCount += 1;
+			}
+		}
+	}
+		printf("Page %d accessed %d -> %d\n", page[pIndex].pid, page[pIndex].accessCount - 1, page[pIndex].accessCount);
 	return false;
 }
 
