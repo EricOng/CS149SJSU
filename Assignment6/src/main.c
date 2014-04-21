4b9062f39ce29c9745d06e2c07326b0a610725a8
Chris Sharpe <csha9933@yahoo.com>
2014-04-21 16:46:58 -0700

    I changed main.c to Mak's example select() program.

--- Assignment6/src/main.c
+++ Assignment6/src/main.c
@@ -1,21 +1,53 @@
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 int main() {
-    fd_set inSet;
+    char buffer[128];
+    int result, nread;
+    fd_set inputs, input_fds;
+    
     struct timeval timeout;
     
-    FD_ZERO(&inSet);
-    FD_SET (open("./resources/file1.txt", O_RDONLY), &inSet);
-
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
+    FD_ZERO(&inputs);
+    FD_SET(0, &inputs);
     
-    printf("%d\n", select(FD_SETSIZE, &inSet, NULL, NULL, &timeout));
+    while(1) {
+    input_fds = inputs;
+    timeout.tv_sec = 2;
+    timeout.tv_usec = 500000;
     
-    return 0;
+    result = select(FD_SETSIZE, &input_fds, NULL, NULL, &timeout);
+    
+    switch(result) {
+	case -1: {
+	    perror("select");
+	    exit(1);
+    	}
+	case 0: {
+	    printf("@");
+	    fflush(stdout);
+	    break;
+	}
+	default: {
+	    if(FD_ISSET(0, &input_fds)) {
+		ioctl(0, FIONREAD, &nread);
+		if(nread == 0) {
+		    printf("Keyboard input done.\n");
+		    exit(0);
+		}
+		nread = read(0, buffer, nread);
+		buffer[nread] = 0;
+		printf("Read %d characters from the keyboard: %s", nread, buffer);
+	    }
+	}
+    }
+    }
 }
 
